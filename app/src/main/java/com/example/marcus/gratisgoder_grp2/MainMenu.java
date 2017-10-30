443fb42f4fd84a432ddbe00dbfb68b58f2f3bd18
Theis Gadegaard <theis.gadegaard@gmail.com>
2017-10-30 11:26:30 +0100

    added MainMenu added pictures "danmarkskort", "headline"

---
+++ app/src/main/java/com/example/marcus/gratisgoder_grp2/MainMenu.java
@@ -0,0 +1,38 @@
+package com.example.marcus.gratisgoder_grp2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
+
+    Button goToFreeStuff, goToAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Button goToFreeStuff = (Button) findViewById(R.id.goToFreeStuff);
+        Button goToAdd = (Button) findViewById(R.id.goToAddStuff);
+
+        goToAdd.setOnClickListener(this);
+        goToFreeStuff.setOnClickListener(this);
+
+        public void onClick(View view){
+        switch(view.getId()) {
+            case R.id.goToAddStuff:
+                System.err.println("not yet implemented");
+                break;
+            case R.id.goToFreeStuff:
+                Intent toolbarIntent = new Intent(this, toolbar.class);
+                startActivity(toolbarIntent);
+                break;
+            default:
+                System.err.println("onClick method undefined");
+        }
+    }
+    }
+}
