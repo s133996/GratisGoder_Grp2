a9e4d75859b59e539d44a2b25119a2d0b624ba5b
Jimmy <tippy@DESKTOP-F71BF8G.eduroam.wireless.dtu.dk>
2017-10-30 11:46:42 +0100

    Adding Toolbar

--- app/src/main/java/com/example/marcus/gratisgoder_grp2/ToolbarActivity.java
+++ app/src/main/java/com/example/marcus/gratisgoder_grp2/ToolbarActivity.java
@@ -2,6 +2,7 @@ package com.example.marcus.gratisgoder_grp2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ToolbarActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class ToolbarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
+
+        Toolbar tb = (Toolbar)findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(tb);
+
     }
 }
