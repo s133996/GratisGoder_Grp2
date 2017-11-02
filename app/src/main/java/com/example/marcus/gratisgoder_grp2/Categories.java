45536cfdb049c8a01934c384d0a78538ba2dc7a2
TheDaneSimon <simon@engquist.dk>
2017-11-02 15:22:04 +0100

    Added map

--- app/src/main/java/com/example/marcus/gratisgoder_grp2/Categories.java
+++ app/src/main/java/com/example/marcus/gratisgoder_grp2/Categories.java
@@ -60,6 +60,8 @@ public class Categories extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_categories, container, false);
+
+
     }
 
 
