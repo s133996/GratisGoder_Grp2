752da84f0dfa8c67ac7a1fc94541cb4eccea7195
Jimmy <tippy_1990@hotmail.com>
2017-11-02 10:36:53 +0100

    Added buttons to toolbar
    Home button works , Search button doesn't work yet
    But we still know when it is being clicked on

--- app/src/main/java/com/example/marcus/gratisgoder_grp2/ToolbarActivity.java
+++ app/src/main/java/com/example/marcus/gratisgoder_grp2/ToolbarActivity.java
@@ -3,6 +3,10 @@ package com.example.marcus.gratisgoder_grp2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ToolbarActivity extends AppCompatActivity {
 
@@ -12,8 +16,35 @@ public class ToolbarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_toolbar);
 
         Toolbar tb = (Toolbar)findViewById(R.id.my_toolbar);
-
         setSupportActionBar(tb);
 
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search_result, menu);
+        return true;
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                finish();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+
+
 }
